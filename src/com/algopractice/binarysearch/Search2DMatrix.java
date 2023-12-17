51751a74540358eab470532ddf3947b759aff0a3
Praveen Bommali <65863196+praveenbommalibits@users.noreply.github.com>
2023-12-17 12:28:35 +0530

    problem-solving practice

--- src/com/algopractice/binarysearch/Search2DMatrix.java
+++ src/com/algopractice/binarysearch/Search2DMatrix.java
@@ -7,21 +7,36 @@ public class Search2DMatrix {
     public static void main(String[] args) {
         Search2DMatrix  matrix = new Search2DMatrix();
         int[][] martixin = new int[][]
-                {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        System.out.println(matrix.searchMatrix(martixin, 3));
+                {{1},{60}};
+        System.out.println(matrix.searchMatrix(martixin, 1));
     }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int totalRows = matrix.length;
-        int c = matrix[0].length;
-        int l =0, r = totalRows-1;
+        int nr = matrix.length;
+        int nc = matrix[0].length;
+        int l =0, r = nr-1;
 
         while(l <= r) {
-            int mid = (l+r)/2;
-            //if(matrix[mid][0][1] < target < matrix[mid][0][c-1] )
-            if(matrix[mid][0] > target)
-                r = mid-1;
+            int row = (r+l)/2;
+            if(target > matrix[row][nc-1])
+                l = row+1;
+            else if (target < matrix[row][0])
+                r = row-1;
             else
-                l = mid+1;
+                break;
+        }
+        if(l<=r) {}
+        else return false;
+        r = (l+r)/2;// single row case
+        int left =0, right = matrix[r].length-1;
+        while(left <= right) {
+
+            int mid = (right+left)/2;
+            if(matrix[r][mid] == target)
+                return true;
+            else if(matrix[r][mid] > target)
+                right = mid-1;
+            else
+                left = mid+1;
         }
 
         return false;
