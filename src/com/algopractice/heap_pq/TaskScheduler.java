715284d5ef15f900465c1fd1e9938f06347487c7
Praveen Bommali <65863196+praveenbommalibits@users.noreply.github.com>
2023-12-30 11:21:34 +0530

    problem-solving practice

--- src/com/algopractice/heap_pq/TaskScheduler.java
+++ src/com/algopractice/heap_pq/TaskScheduler.java
@@ -2,6 +2,29 @@ package com.algopractice.heap_pq;
 
 import java.util.*;
 
+/**
+ *
+ 1. Valid Parentheses: https://lnkd.in/gjfmc-sR
+
+ 2. Merge Intervals: https://lnkd.in/gPMQWcKV
+
+ 3. Best Time to Buy and Sell Stock: https://lnkd.in/gzyaCJeD
+
+ 4. Combination Sum: https://lnkd.in/g8G7Ad69
+
+ 5. Number of Islands: https://lnkd.in/gPhFiieJ
+
+ 6. Group Anagrams: https://lnkd.in/gxUFWtaE
+
+ 7. LRU Cache: https://lnkd.in/gG2CNNfN
+
+ 8. Longest Substring Without Repeating Characters: https://lnkd.in/gAMzN3jV
+
+ 9. Basic Calculator: https://lnkd.in/gXZQzQnK
+
+ 10. Two Sum: https://lnkd.in/g9Vidkj4
+
+ */
 public class TaskScheduler {
     public static void main(String[] args) {
         TaskScheduler taskScheduler = new TaskScheduler();
@@ -18,11 +41,14 @@ public class TaskScheduler {
         maxHeap.addAll(cCount.values());
         Queue<int[]> trackQueue = new LinkedList<>();
         int totalTime = 0;
-        while(!maxHeap.isEmpty()) {
+        while(!maxHeap.isEmpty() ||
+                !trackQueue.isEmpty()) {
             totalTime += 1;
-            int remainTaskCount = maxHeap.poll()-1;
-            if(remainTaskCount >0) {
-                trackQueue.add(new int[]{remainTaskCount, totalTime+n});
+            if(!maxHeap.isEmpty()) {
+                int remainTaskCount = maxHeap.poll() - 1;
+                if (remainTaskCount > 0) {
+                    trackQueue.add(new int[]{remainTaskCount, totalTime + n});
+                }
             }
 
             if(!trackQueue.isEmpty() && trackQueue.peek()[1]==totalTime) {
