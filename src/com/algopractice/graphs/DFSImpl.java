a68c86dc1a42fe993c64f0593b212ce1ab88298c
Praveen Bommali <65863196+praveenbommalibits@users.noreply.github.com>
2024-01-11 07:34:24 +0530

    problem-solving practice

---
+++ src/com/algopractice/graphs/DFSImpl.java
@@ -0,0 +1,12 @@
+package com.algopractice.graphs;
+
+public class DFSImpl {
+
+    public static void main(String[] args) {
+        DFSImpl dfsimpl = new DFSImpl();
+        int nodes = 6, edges=7;
+
+    }
+
+
+}
